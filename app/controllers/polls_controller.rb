54cd72352156e6794ade91cf581b373ffbf81a19
Nauzet <nauzet18@gmail.com>
2015-07-13 08:57:04 +0000

    añadido el controlador polls y adaptado el enrutador para este recurso

---
+++ app/controllers/polls_controller.rb
@@ -0,0 +1,10 @@
+class PollsController < ApplicationController
+  def new
+  end
+
+  def edit
+  end
+
+  def index
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+
+  resources :polls, except: :show
+  resources :groups, except: :show
+  resources :questions, except: :show
+  resources :answers, except: :show
+
+
+  root to: "polls#index"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
