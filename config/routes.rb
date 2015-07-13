8b252d05d17ec8786c78b20e707b61f660e7ec30
Nauzet <nauzet18@gmail.com>
2015-07-13 10:54:23 +0000

    Modificado el enrutador para añadir los recursos de los controladores q faltaban

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,6 @@ Rails.application.routes.draw do
   resources :questions, except: :show
   resources :answers, except: :show
 
-
   root to: "polls#index"
 
   # The priority is based upon order of creation: first created -> highest priority.
