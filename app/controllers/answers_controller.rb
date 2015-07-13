50056fd7372ba7b7937fd945f42282a49384d028
Nauzet <nauzet18@gmail.com>
2015-07-13 10:53:04 +0000

    Añadidos los controladores que faltan y sus vistas

---
+++ app/controllers/answers_controller.rb
@@ -0,0 +1,10 @@
+class AnswersController < ApplicationController
+  def new
+  end
+
+  def edit
+  end
+
+  def index
+  end
+end
---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,10 @@
+class QuestionsController < ApplicationController
+  def new
+  end
+
+  def edit
+  end
+
+  def index
+  end
+end
