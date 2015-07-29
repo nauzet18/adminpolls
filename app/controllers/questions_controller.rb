a801a364d1441385744b51b074d90c8af92c1249
Abby Henríquez Tejera <paradoja@gmail.com>
2015-07-29 22:14:01 +0200

    Cambios porque sí

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,42 +1,51 @@
 class QuestionsController < InheritedResources::Base
   def create
-    create! do |success, failure|
-
+    create! do |success, _failure|
       success.html do
-        redirect_to questions_path, notice: I18n.t( :saved )
+        redirect_to questions_path, notice: I18n.t(:saved)
       end
-
     end
   end
 
   def update
-    update! do |success, failure|
-
+    update! do |success, _failure|
       success.html do
-        redirect_to questions_path, notice: I18n.t( :saved )
+        redirect_to questions_path, notice: I18n.t(:saved)
       end
-
     end
   end
-  
+
   private
 
   def collection
+    @contents =
+      if param_sizes(search_text: :nonzero?, search_select: :nonzero?)
+        end_of_association_chain
+          .where("name ilike ? AND group_id = ?", "%#{params[:search_text]}%", params[:search_select].to_i)
+      elsif param_sizes(search_text: :zero?, search_select: :nonzero?)
+        end_of_association_chain
+          .where("group_id = ?", params[:search_select])
+      elsif params(search_text: :nonzero?, search_select: :zero?)
+        end_of_association_chain
+          .where("name ilike ?", "%#{params[:search_text]}%")
+      else
+        end_of_association_chain
+      end.order(:id).page( params[:page])
+  end
 
-    if params[:search_text] and params[:search_text].size > 0 and params[:search_select] and params[:search_select].size > 0
-      @contents = end_of_association_chain.where("name ilike ? AND group_id = ?", "%#{params[:search_text]}%", Integer(params[:search_select]) ).order(:id).page( params[:page] )
-    elsif params[:search_text] and params[:search_text].size == 0 and params[:search_select] and params[:search_select].size > 0
-      @contents = end_of_association_chain.where("group_id = ?", Integer(params[:search_select]) ).order(:id).page( params[:page] )
-    elsif params[:search_text] and params[:search_text].size > 0 and params[:search_select] and params[:search_select].size == 0
-      @contents = end_of_association_chain.where("name ilike ?", "%#{params[:search_text]}%").order(:id).page( params[:page] )
-    else
-      @contents = end_of_association_chain.order(:id).page( params[:page] )
-    end
-
+  def param_sizes(**params)
+    params.map do |key, proc|
+      params[key].try(proc)
+    end.all?
   end
 
   def question_params
-    params.require(:question).permit(:page, :name, :group_id, answers_attributes: [:id, :name, :correct, :_destroy]).tap do |whitelisted|
+    params.require(:question).permit(
+      :page,
+      :name,
+      :group_id,
+      answers_attributes: %i{id name correct _destroy}
+    ).tap do |whitelisted|
       whitelisted[:page] = params[:page]
     end
   end
